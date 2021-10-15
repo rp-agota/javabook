d19d995b761fb41e4c8ebe99af8344310a284c57
Aoi Kondo <agotadmidori@gmail.com>
2021-10-15 10:30:21 +0900

    1,2

---
+++ Section2/P1.java
@@ -0,0 +1,7 @@
+public class P1{
+    public static void main(String[] args) {
+        System.out.println("氏名: (自分の名前)");
+        System.out.println("住所: (自分の住所)");
+        System.out.println("生年月日: 1945年8月15日");
+    }
+}
---
+++ Section2/P2.java
@@ -0,0 +1,11 @@
+public class P2{
+    public static void main(String[] args) {
+        System.out.println("　　　　　　プ");
+        System.out.println("　　　　　プロ");
+        System.out.println("　　　　プログ");
+        System.out.println("　　　プログラ");
+        System.out.println("　　プログラミ");
+        System.out.println("　プログラミン");
+        System.out.println("プログラミング");
+    }
+}
